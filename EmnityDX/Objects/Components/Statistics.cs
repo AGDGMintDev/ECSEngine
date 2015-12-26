33feed5b9a46c5678eabc6afe30f97e7fc1f8fb8
AGDGMintDev <agdgmintdev@gmail.com>
2015-12-25 21:19:52 -0500

    add new component and level for testing.

---
+++ EmnityDX/Objects/Components/Statistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmnityDX.Objects.Components
+{
+    public struct Statistics
+    {
+        public double Value;
+        public double Rarity;
+        public double PhysicalDamage;
+        public double MentalDamage;
+        public string SpecialEffects;
+        public string FlavorText;
+    }
+}
---
+++ EmnityDX/Objects/LevelData/PlayingStateLevels/RandomGeneration.cs
@@ -0,0 +1,18 @@
+﻿using EmnityDX.Engine;
+using EmnityDX.Objects.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using EmnityDX.Objects.LevelData.PauseStateLevels;
+
+namespace EmnityDX.Objects.LevelData.PlayingStateLevels
+{
+    public class RandomGeneration: Level
+    {
+    }
+}
